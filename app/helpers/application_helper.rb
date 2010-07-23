defa79a45980e1d58bf8dd4d38a618146c3f180b
DBA <diogo.borges.almeida@gmail.com>
2010-07-23 11:20:56 +0100

    Page titles   - added the title application helper and tests [closes #47]   - edited the homepage and about page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  # Sets the page title variable
+  def title(title = nil, append_company = false)
+    @title = title.present? ? title.to_s : 'Gnomeslab'
+    @title << ' | Gnomeslab' if append_company
+    @title
+  end
+
   # Returns the content page title for a given text as a raw html string.
   #
   # Note: If the provided title is not HTML safe you will endanger the site's security!
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 context ApplicationHelper do
+  describe "page title" do
+    it "returns gnomeslab by default" do
+      title.should == 'Gnomeslab'
+    end
+
+    it "allows its value to be overridden" do
+      title('howdy').should == 'howdy'
+    end
+
+    it "can also append ' - Gnomeslab' to the title" do
+      title('Blog', true).should == 'Blog | Gnomeslab'
+    end
+  end
+
   describe "content title" do
     it "should return the content title html for the provided text" do
       content_title('hello world').should == "<h2 class='border'>hello world</h2>"
