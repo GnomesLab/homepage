edab8c5df445825ce1ab49cdeb8dcd063174a08b
Miguel Teixeira <miguelfteixeira@gmail.com>
2010-07-27 22:12:03 +0100

    - added project factory

---
+++ spec/factories/project_factory.rb
@@ -0,0 +1,6 @@
+Factory.define :project do |p|
+  p.sequence(:title)    { |n| "project#{n} title"                 }
+  p.sequence(:subtitle) { |n| "project#{n} subtitle"              }
+  p.sequence(:image)    { |n| "../images/content/project#{n}.jpg" }
+  p.sequence(:date)     { |n| n.weeks.ago.to_date                 }
+end
