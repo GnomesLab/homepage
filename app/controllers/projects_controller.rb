3e84e759e741579d7886d250c544fa74b97fbb8a
Miguel Teixeira <miguelfteixeira@gmail.com>
2010-07-28 01:00:35 +0100

    - started using will_paginate

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
   def index
+    Project.recent.paginate :page => params[:page], :per_page => Project.per_page
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+  # Pagination
+  cattr_reader :per_page
+  @@per_page = 5
+
+  # Named scopes
+  scope :recent, order('date desc')
 end
