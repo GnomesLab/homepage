7dac92293adc061d914fc1510b39779e49c9aa33
DBA <diogo.borges.almeida@gmail.com>
2010-07-29 20:02:42 +0100

    User spec   - reviewed pedro's user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,43 @@
 require 'spec_helper'
 
-describe User do
-  
-  subject { Factory.build(:user) }
-  
-  it "must accept a valid name" do
-    subject.name = "Pedro Coutinho"
-    subject.should be_valid
-  end
-  
-  it "it must not let 2 small names pass" do
-    subject.name = "Pe Co"
-    subject.should_not be_valid
-  end
-  
-  it "it must not let 1 big name pass" do
-    subject.name = "PedroCoutinho"
-    subject.should_not be_valid
-  end
-  
-  it "must have a name" do
-    subject.name = nil
-    subject.should_not be_valid
-  end
-  
-  it "must accept a valid email" do
-    subject.email = "pdcoutinho@gmail.com"
-    subject.should be_valid
-  end
-  
-  it "must have an email" do
-    subject.email = nil
-    subject.should_not be_valid
-  end
-  
-  it "must have a valid email" do
-    subject.email = "invalid email"
-    subject.should_not be_valid
-  end
-  
-  it "must must accept a valid password" do
-    subject.password = "123456"
-    subject.should be_valid
-  end
-  
-  it "must have a password" do
-    subject.password = nil
-    subject.should_not be_valid
-  end
-  
-  it "password must be greater than 5 digits" do
-    subject.password = "123"
-    subject.should_not be_valid
-  end
+context User do
 
-  
+  subject { Factory.build(:user) }
+
+  describe "validations" do
+
+    it "recognises valid input data" do
+      subject.should be_valid
+    end
+
+    describe "name" do
+      it "defines name as a required attribute" do
+        subject.name = nil
+        subject.should_not be_valid
+      end
+
+      it "doesn't support two small names" do
+        subject.name = "Pe Co"
+        subject.should_not be_valid
+      end
+
+      it "doesn't support one big name" do
+        subject.name = "PedroCoutinho"
+        subject.should_not be_valid
+      end
+    end # name
+
+    describe "devise" do
+      it "enforces devise standard validations" do
+        subject.email = nil
+        subject.password = "123"
+
+        subject.should_not be_valid
+        subject.errors[:email].should_not be_nil
+        subject.errors[:password].should_not be_nil
+      end
+    end #devise
+
+  end # Validations
+
 end
