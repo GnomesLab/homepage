406138a20dcb904c7d3d40a30ccf3d1485865224
Miguel Teixeira <miguelfteixeira@gmail.com>
2010-07-29 22:23:00 +0100

    - stated the implementation of project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 context Project do
-  pending 'Miguel should be doing more BDD and less Code Driven Development / Hope Driven Development!...'
+  subject { Factory.build(:project) }
 
-  # subject { Factory.build(:project) }
+  describe "validations" do
+  end # validations
 
-  # describe "validations" do
-  # end # validations
+  describe "named scopes" do
+    before(:each) do
+      10.times.each { Factory.create(:project) }
+    end
+
+    it "reverse named scope must order the projects by date desc" do
+      Project.reverse.should == Project.order('date desc')
+    end
+  end
+
+  describe "pagination" do
+    it "should only display 5 projects per page" do
+      Project.per_page.should == 5
+    end
+  end
 
 end
