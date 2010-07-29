756a1746f44572c67dd3c9564798a9f2763ddc8e
DBA <diogo.borges.almeida@gmail.com>
2010-07-29 20:05:28 +0100

    Enquiry spec   - semantic update

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe Enquiry do
+context Enquiry do
 
   subject { Factory.build(:enquiry) }
 
-  describe "properties" do
+  describe "validations" do
     it "must have a name" do
       subject.name = nil
       subject.should_not be_valid
@@ -33,5 +33,6 @@ describe Enquiry do
     it "should be valid with all the properties set" do
       subject.should be_valid
     end
-  end
+  end # Validations
+
 end
