8f5c3b98c1a65946ad9e2f650f7f2db900cedafc
DBA <diogo.borges.almeida@gmail.com>
2010-07-29 20:08:29 +0100

    Project spec   - added project spec for Miguel (BDD MIGUEL, BDD!)

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+context Project do
+  pending 'Miguel should be doing more BDD and less Code Driven Development / Hope Driven Development!...'
+
+  # subject { Factory.build(:project) }
+
+  # describe "validations" do
+  # end # validations
+
+end
