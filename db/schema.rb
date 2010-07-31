479e8caeb16c699aed1c178b8ba72f7616143109
Miguel Teixeira <miguelfteixeira@gmail.com>
2010-07-31 13:29:14 +0100

    - added count_of_projects migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100729233314) do
+ActiveRecord::Schema.define(:version => 20100731121505) do
 
   create_table "categories", :force => true do |t|
-    t.string   "name",       :null => false
+    t.string   "name",              :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "count_of_projects"
   end
 
   create_table "enquiries", :force => true do |t|
