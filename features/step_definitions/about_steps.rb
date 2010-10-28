f732e842df15f22c01e0fcd69e7454915b0f922f
pdcoutinho <pedro.coutinho@gnomeslab.com>
2010-10-28 15:49:48 +0100

    - Fixed deprecated messages from cucumber

--- features/step_definitions/about_steps.rb
+++ features/step_definitions/about_steps.rb
@@ -2,12 +2,12 @@ Then /^I should see (.*) Linkedin profile link$/ do |handle|
   element = get_html_element(handle, 'linkedin')
 
   element.text.should == 'LinkedIn profile'
-  element.node.attributes['href'].value.should == "http://pt.linkedin.com/in/#{handle}"
+  element.native.attributes['href'].value.should == "http://pt.linkedin.com/in/#{handle}"
 end
 
 Then /^I should see (.*) Twitter link$/ do |handle|
   element = get_html_element(handle, 'twitter')
 
   element.text.should == 'follow me on Twitter'
-  element.node.attributes['href'].value.should == "http://twitter.com/#{handle}"
+  element.native.attributes['href'].value.should == "http://twitter.com/#{handle}"
 end
