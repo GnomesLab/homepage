3bb795a9252366545c5eafeed7e06b5aec87f7d9
DBA <diogo.almeida@gnomeslab.com>
2010-10-29 09:35:49 +0100

    Post   - recent is now a named scope instead of a class method

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,7 @@ class Post < ActiveRecord::Base
   scope :latest,    order('published_at DESC')
   scope :published, where('published_at IS NOT NULL')
   scope :popular,   lambda { |l = 5| published.order('views DESC').limit(l) }
+  scope :recent,    lambda { |l = 5| published.latest.limit(l) }
 
   # Public class methods
   #
@@ -56,16 +57,6 @@ class Post < ActiveRecord::Base
   def second_level_comments(parent, all = false)
     all ? self.comments.second_level(parent) : self.comments.second_level(parent).visible
   end
-
-  # returns the 5 most recent posts
-  #
-  # this method relies on the scope :latest
-  #
-  # View example: (on recent posts partial)
-  #   <% recent.each do |p| %>
-  def self.recent(limit = 5)
-    self.published.latest.limit(limit)
-  end # end recent (posts)
 
   # returns up to 5 related posts
   #
